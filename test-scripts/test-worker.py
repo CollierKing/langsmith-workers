6177f5020360a2019017b92ade769412c40e0ab3
Collier King <collier@cloudflare.com>
2025-07-14 10:52:02 -0500

    create/updateRun

--- test-scripts/test-worker.py
+++ test-scripts/test-worker.py
@@ -2,10 +2,10 @@ import requests
 
 url = "http://localhost:8787/query"
 
-r = requests.post(url, json={"agentId": "test-user-123", "prompt": "Add a task to buy groceries"})
+r = requests.post(url, json={"agentId": "test-user-123", "prompt": "Add a task to get the mail"})
 print(r.content)
 
-r = requests.post(url, json={"agentId": "test-user-123", "prompt": "List all my tasks"})
-
-r
-print(r.content)
+# r = requests.post(url, json={"agentId": "test-user-123", "prompt": "List all my tasks"})
+#
+# r
+# print(r.content)
